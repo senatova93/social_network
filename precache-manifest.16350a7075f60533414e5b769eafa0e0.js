5af0c08ffe7dce576d55375435b3c3d0dcb9d989
Senatova93 <nanniin160@gmail.com>
2020-03-29 16:36:54 +0300

    Updates

---
+++ precache-manifest.16350a7075f60533414e5b769eafa0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9769b0c5adc64e812fade544dbc74c93",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "6155c511206d34d455eb",
+    "url": "/social_network/static/css/main.0e9ff181.chunk.css"
+  },
+  {
+    "revision": "d5d18e3b2a370ba128c4",
+    "url": "/social_network/static/js/2.fac88ef9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/social_network/static/js/2.fac88ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6155c511206d34d455eb",
+    "url": "/social_network/static/js/main.430659d4.chunk.js"
+  },
+  {
+    "revision": "3618d427b38289bc0d37",
+    "url": "/social_network/static/js/runtime-main.293f3b61.js"
+  }
+]);
