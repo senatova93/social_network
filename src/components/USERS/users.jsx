0f4799d5a46310d610f054c2af0e7b514954a3dc
Senatova93 <nanniin160@gmail.com>
2020-05-30 09:21:21 +0300

    side effect

--- src/components/USERS/users.jsx
+++ src/components/USERS/users.jsx
@@ -4,14 +4,16 @@ import * as axios  from "axios";
 import userPhoto from '../../assets/images/users.png'
 
 let Users = (props) => {
-    if (props.users.length ===0){
-    axios.get(" https://social-network.samuraijs.com/api/1.0/users").then(response => {
-
+    let getUsers = () =>
+    { if (props.users.length ===0){
+    axios.get(" https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
         props.setUsers(response.data.items)
     })
 
-    }
+    }}
     return <div>
+        <button onClick= {getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
