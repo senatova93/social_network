5f51c834d0b3aa2dec0324185c9268a5e4966184
Senatova93 <nanniin160@gmail.com>
2020-05-30 08:50:37 +0300

    API

--- src/components/USERS/users.jsx
+++ src/components/USERS/users.jsx
@@ -1,44 +1,15 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios  from "axios";
 
 let Users = (props) => {
     if (props.users.length ===0){
-    props.setUsers([
+    axios.get(" https://social-network.samuraijs.com/api/1.0/users").then(response => {
 
-        {
-            id: 1,
-            photoURL: 'https://ont.by/static/command/main/cell-big/tYzSgwm4n72WxjBw.jpg',
-            followed: true,
-            fullName: 'Nannii',
-            status: 'Lucky person',
-            location: {city: 'Florida', country: 'USA'}
-        },
-        {
-            id: 2,
-            photoURL: 'https://ont.by/static/command/main/cell-big/tYzSgwm4n72WxjBw.jpg',
-            followed: true,
-            fullName: 'Vew',
-            status: 'Lucky person too',
-            location: {city: 'Moscow', country: 'Russia'}
-        },
-        {
-            id: 3,
-            photoURL: 'https://ont.by/static/command/main/cell-big/tYzSgwm4n72WxjBw.jpg',
-            followed: false,
-            fullName: 'Masha',
-            status: 'Undefenetly no',
-            location: {city: 'Ukrain', country: 'Kiev'}
-        },
-        {
-            id: 4,
-            photoURL: 'https://ont.by/static/command/main/cell-big/tYzSgwm4n72WxjBw.jpg',
-            followed: false,
-            fullName: 'Marik',
-            status: 'Super-boy',
-            location: {city: 'Florida', country: 'USA'}
-        },
+        props.setUsers(response.data.items)
+    })
 
-    ])}
+    }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
