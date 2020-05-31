aa94cd504e6601a056522f8c49573cd01c36aa70
Senatova93 <nanniin160@gmail.com>
2020-05-31 09:08:08 +0300

    class component

--- src/components/USERS/users.jsx
+++ src/components/USERS/users.jsx
@@ -4,19 +4,16 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/users.png'
 
 class Users extends React.Component {
-        getUsers = () => {
-            if (this.props.users.length === 0) {
-                axios.get(" https://social-network.samuraijs.com/api/1.0/users")
-                    .then(response => {
-                       this.props.setUsers(response.data.items)
-                    })
+    constructor(props) {
+        super(props);
+            axios.get(" https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                })
 
-            }
-        }
-
+    }
     render() {
         return <div>
-            <button onClick={this.getUsers}>Get Users</button>
             {
                this.props.users.map(u => <div key={u.id}>
                 <span>
